974cb67ef98dc5dd2cf868eaf1e48abb464fcf93
Chris Sinjakli <chris@sinjakli.co.uk>
2015-12-08 21:58:08 +0000

    Create ~/.config/awesome/ if it doesn't exist

--- symlink_ubuntu.sh
+++ symlink_ubuntu.sh
@@ -6,6 +6,7 @@ set -eu
 
 if [ ! -e "$HOME/.config/awesome/rc.lua" ]
 then
+  mkdir -p $HOME/.config/awesome
   ln -s $PWD/config/ubuntu/awesome/rc.lua $HOME/.config/awesome/rc.lua
 else
   echo "$HOME/.config/awesome/rc.lua already exists" >&2
