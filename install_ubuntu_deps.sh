ffda08a49c63ff678aa7710b0e8cc36fcb16c473
Chris Sinjakli <chris@sinjakli.co.uk>
2016-03-20 19:24:02 +0000

    Install exuberant-ctags and silversearcher-ag as vim-config needs them

--- install_ubuntu_deps.sh
+++ install_ubuntu_deps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-apt-get install awesome awesome-extra git tmux vim-nox
+apt-get install awesome awesome-extra exuberant-ctags git silversearcher-ag tmux vim-nox
 apt-get remove unity-webapps-common
