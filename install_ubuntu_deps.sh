58c2b1b127f1d661d81ec42dd547f17fc778e58d
Chris Sinjakli <chris@sinjakli.co.uk>
2016-03-20 23:25:35 +0000

    Ensure gnome-settings-daemon is installed

--- install_ubuntu_deps.sh
+++ install_ubuntu_deps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-apt-get install awesome awesome-extra exuberant-ctags git silversearcher-ag tmux vim-nox
+apt-get install awesome awesome-extra exuberant-ctags git gnome-settings-daemon silversearcher-ag tmux vim-nox
 apt-get remove unity-webapps-common
