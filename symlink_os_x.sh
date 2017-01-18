3c31be260aaac1d25d749bbab1669e9e241b4fa9
Chris Sinjakli <chris@sinjakli.co.uk>
2017-01-18 11:22:39 +0000

    Copy color palettes rather than symlinking as OS X ignores symlinks
    Why must all software be bad? :(

--- symlink_os_x.sh
+++ symlink_os_x.sh
@@ -35,7 +35,9 @@ fi
 if [ ! -e "$HOME/Library/Colors/Sinjo.clr" ]
 then
   mkdir -p $HOME/Library/Colors
-  ln -s $PWD/config/os_x/colors/Sinjo.clr $HOME/Library/Colors/Sinjo.clr
+  cp $PWD/config/os_x/colors/Sinjo.clr $HOME/Library/Colors/Sinjo.clr
 else
-  echo "$HOME/Library/Colors/Sinjo.clr already exists" >&2
+  (cmp --quiet $HOME/Library/Colors/Sinjo.clr $PWD/config/os_x/colors/Sinjo.clr &&
+    echo "$HOME/Library/Colors/Sinjo.clr already exists" >&2) ||
+    echo "$HOME/Library/Colors/Sinjo.clr differs from $PWD/config/os_x/colors/Sinjo.clr, update either repo or Library version"
 fi
